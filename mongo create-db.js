0ac36747ccc6bedd65e7b545f59b08dfcd78e58f
roshanprenny <56357611+roshanprenny@users.noreply.github.com>
2022-02-26 13:05:51 +0530

    Create mongo create-db.js

---
+++ mongo create-db.js
@@ -0,0 +1,23 @@
+MongoDB shell version: 2.6.5
+connecting to: test
+connecting to: 127.0.0.1:27017/madMen
+* Database created
+* Documents created
+* All documents:
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fb"), "name" : "Don Draper" }
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fc"), "name" : "Peter Campbell" }
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fd"), "name" : "Betty Draper" }
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fe"), "name" : "Joan Harris" }
+* Updated document: 550f2939a8a5d2643f6408fb
+* All documents:
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fb"), "name" : "Dick Whitman" }
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fc"), "name" : "Peter Campbell" }
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fd"), "name" : "Betty Draper" }
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fe"), "name" : "Joan Harris" }
+* Removed document:
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fb"), "name" : "Dick Whitman" }
+* All documents ("Dick Whitman" removed):
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fc"), "name" : "Peter Campbell" }
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fd"), "name" : "Betty Draper" }
+{ "_id" : ObjectId("550f2939a8a5d2643f6408fe"), "name" : "Joan Harris" }
+* Database dropped
